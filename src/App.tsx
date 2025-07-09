a0b8bd2e13d4241efe68c8ead6ea3a73b148ad56
Rajeev Ranjan Kumar <rajeev.roy@zohomail.in>
2025-07-09 15:52:47 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8">
+      <p className="text-center text-sm sm:text-base md:text-lg lg:text-xl max-w-md sm:max-w-lg md:max-w-xl">
+        Start prompting (or editing) to see magic happen :)
+      </p>
+    </div>
+  );
+}
+
+export default App;
